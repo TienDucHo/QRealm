c72333f4271e81cca3af881ffb6b5bfbfd8cf5c2
Duc Ho <hotienduc05012002@gmail.com>
2023-03-12 19:07:09 -0600

    Added Image Capture Use Case

--- java/app/src/main/java/com/llamoss/qrealm/MainActivity.java
+++ java/app/src/main/java/com/llamoss/qrealm/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.core.impl.CameraConfig;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -16,11 +17,13 @@ import androidx.lifecycle.LifecycleOwner;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -29,20 +32,27 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.llamoss.qrealm.databinding.ActivityMainBinding;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_CAMERA = 0;
     private ActivityMainBinding binding;
     private ImageCapture imageCapture;
     private VideoCapture<Recorder> videoCapture;
     private Recording recording;
+    private final String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
+
+    private final String TAG = "CameraX";
     private ExecutorService cameraExecutor;
-    private final ArrayList<String> REQUESTED_PERMISSIONS = new ArrayList<String>();
+    private final ArrayList<String> REQUESTED_PERMISSIONS = new ArrayList<>(
+            Arrays.asList(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO
+            )
+    );
 
     private final int REQUEST_CODE = 10;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
@@ -52,11 +62,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate((getLayoutInflater()));
         setContentView(binding.getRoot());
-        binding.imageCaptureButton.setOnClickListener(v -> takePhoto());
-        binding.videoCaptureButton.setOnClickListener(v -> captureVideo());
-        cameraExecutor = Executors.newSingleThreadExecutor();
-        REQUESTED_PERMISSIONS.addAll((Arrays.asList(Manifest.permission.CAMERA,
-                Manifest.permission.RECORD_AUDIO)));
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P)
         {
             REQUESTED_PERMISSIONS.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
@@ -69,9 +74,42 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this,
                     REQUESTED_PERMISSIONS.toArray(new String[0]), REQUEST_CODE);
         }
+
+        binding.imageCaptureButton.setOnClickListener(v -> takePhoto());
+        binding.videoCaptureButton.setOnClickListener(v -> captureVideo());
+        cameraExecutor = Executors.newSingleThreadExecutor();
     }
 
-    private void takePhoto() {}
+    private void takePhoto() {
+        String name = new SimpleDateFormat(FILENAME_FORMAT, Locale.CANADA)
+                .format(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P)
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(getContentResolver(),
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues).build();
+        imageCapture.takePicture(
+                outputFileOptions, ContextCompat.getMainExecutor(this),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        final String msg = "Photo capture succeeded: " +
+                                outputFileResults.getSavedUri();
+                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+                        Log.d(TAG, msg);
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Log.e(TAG, "Photo capture failed: " + exception);
+                    }
+                }
+        );
+    }
 
     private void captureVideo() {}
 
@@ -98,8 +136,17 @@ public class MainActivity extends AppCompatActivity {
                         .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                         .build();
 
+                imageCapture = new ImageCapture.Builder().build();
+
                 preview.setSurfaceProvider(binding.viewFinder.getSurfaceProvider());
-                cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+                try {
+                    cameraProvider.unbindAll();
+                    cameraProvider.bindToLifecycle((LifecycleOwner)this,
+                            cameraSelector, preview, imageCapture);
+                } catch (Exception exception)
+                {
+                    Log.e(TAG, "Use case binding failed" + exception);
+                }
             } catch (ExecutionException | InterruptedException e) {
                 // No errors need to be handled for this Future.
                 // This should never be reached.
@@ -124,11 +171,15 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this,
                         "Permissions not granted by the user.",
                         Toast.LENGTH_LONG).show();
-                finish();
             }
         }
         // Other 'case' lines to check for other
         // permissions this app might request.
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
+    }
 }
